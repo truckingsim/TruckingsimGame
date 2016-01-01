e08b5f1ce6ef354292e761db466454907cdf8b40
Adam Heim <jefffan24@gmail.com>
2016-01-01 17:43:45 -0500

    Clean up example views into a more reasonable orginization.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,11 +2,11 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>{{ isset($title) ? $title . ' - ' : null }}Laravel - The PHP Framework For Web Artisans</title>
+    <title>{{ isset($title) ? $title . ' - ' : null }}Truckingsim Game</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="author" content="Taylor Otwell">
-    <meta name="description" content="Laravel - The PHP framework for web artisans.">
-    <meta name="keywords" content="laravel, php, framework, web, artisans, taylor otwell">
+    <meta name="description" content="Truckingsim Game">
+    <meta name="keywords" content="truckingsim, trucking">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--[if lte IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -15,8 +15,6 @@
     <link rel="apple-touch-icon" href="/favicon.png">
 </head>
 <body>
-
-<span class="overlay"></span>
 
 <div class="navbar navbar-default">
     <div class="container">
